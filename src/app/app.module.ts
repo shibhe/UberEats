a1ce885399282e8ce216532fa831ef77706d7424
Joseph <sibiyajvt@gmail.com>
2017-10-23 14:13:45 +0200

    First Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MainComponent } from './body/main/main.component';
+import { HeaderComponent } from './body/header/header.component';
+import { FooterComponent } from './body/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
