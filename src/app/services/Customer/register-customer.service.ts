d295b8468baca531b45b6eb7cab7d0ee17388bbd
Joseph <sibiyajvt@gmail.com>
2017-11-06 15:48:41 +0200

    dgfdfd
    dfffs

--- src/app/services/Customer/register-customer.service.ts
+++ src/app/services/Customer/register-customer.service.ts
@@ -20,17 +20,15 @@ export class RegisterCustomerService {
     login(username: string, password: string) {
       return this.http.post(`${this.BASE_URL}/login.php`, JSON.stringify({ username: username, password: password }), { headers: this.headers })
           .map((response: Response) => {
-              // login successful if there's a jwt token in the response
               let user = response.json();
               if (user && user.token) {
-                  // store user details and jwt token in local storage to keep user logged in between page refreshes
-                  localStorage.setItem('currentUser', JSON.stringify(user));
+                  localStorage.setItem('email', JSON.stringify(user));
               }
           });
   }
 
   logout() {
       // remove user from local storage to log user out
-      localStorage.removeItem('currentUser');
+      localStorage.removeItem('email');
   }
 }
